04440b4624da69577bc725dc7ae89f69767da1a4
parkhwakyoung <firenoah.m@gmail.com>
2023-03-06 20:45:58 +0900

    평균 구하기

--- noah/src/noah/Average.java
+++ noah/src/noah/Average.java
@@ -1,10 +1,29 @@
 package noah;
 
+import java.util.Scanner;
+
 public class Average {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String args[]) {
+		int a[]= new int [5];
+		double avg=0;
+		int sum=0;
+		
+		Scanner scan=new Scanner(System.in);
+		
+		for(int i=0; i<a.length; i++) {
+			System.out.print((i+1)+"번째 수 : ");
+			a[i]=scan.nextInt();
+			
+		}
+				
+		for(int i=0; i<a.length; i++) {
+			
+			sum+=a[i];
+		}
+		avg=sum/(a.length);
+		System.out.println("===============");
+		System.out.printf("평균: %.2f",avg);
 	}
 
 }
+
