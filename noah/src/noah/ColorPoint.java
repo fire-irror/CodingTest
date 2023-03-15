fd04d705024337fbb5bf76bc9d4b4ca7780ea968
parkhwakyoung <firenoah.m@gmail.com>
2023-03-15 11:49:05 +0900

    주석 추가

--- noah/src/noah/ColorPoint.java
+++ noah/src/noah/ColorPoint.java
@@ -4,11 +4,11 @@ public class ColorPoint extends Point {
 
 	private String color;
 
-	public void setColor(String color) {
-		this.color = color;
+	public void setColor(String color) {	//점의 색을 저장
+		this.color = color;	//color에 "red"저장
 	}
 
-	public void showColorPoint() {
+	public void showColorPoint() {	
 		System.out.print(color);
 		showPoint();
 	}
--- noah/src/noah/ColorPointEx.java
+++ noah/src/noah/ColorPointEx.java
@@ -4,10 +4,17 @@ public class ColorPointEx {
 
 	public static void main(String[] args) {
 		Point p=new Point();
+		//Point클래스를 가리키는 객체변수 p에 의해 객체 생성
+		//그 객체는 p이름으로 접근한다. 
 		p.set(1, 2);
+		//Point 클래스 안에 있는 set()메소드를 호출한다. 
 		p.showPoint();
+		//ShowPoint메소드를 호출한다. 
 		
-		ColorPoint cp=new ColorPoint();
+		ColorPoint cp = new ColorPoint();
+		//ColorPoint클래스를 가리키는 객체 생성
+		//cp라는 이름으로 접근하고, point 클래스를 상속받았기 때문에 
+		//Point클래스의 멤버에 접근 가능하다. 
 		cp.set(3, 4);
 		cp.setColor("red");
 		cp.showColorPoint();
