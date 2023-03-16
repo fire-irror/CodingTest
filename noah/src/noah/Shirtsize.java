483bafdb7dd9844e62d8b935e7c9d45f46b0cd76
parkhwakyoung <firenoah.m@gmail.com>
2023-03-16 16:59:49 +0900

    rank 구현중

--- noah/src/noah/Shirtsize.java
+++ noah/src/noah/Shirtsize.java
@@ -6,7 +6,10 @@ import java.util.Arrays;
 몇 벌씩 필요한지 가장 작은 사이즈부터 순서대로 배열에 담아 return 하게 하라.*/
 
 class Solution {
-	public int[] Solution(String[] shirtSize) {
+	public int[] solution(String[] shirtSize) {
+		int rank[]=new int[6];
+
+		
 
 		int[] shirt_count = new int[6];
 		for (int i = 0; i < shirtSize.length; i++) {
@@ -14,13 +17,20 @@ class Solution {
 				shirt_count[0]++;
 			} else if (shirtSize[i].equals("S")) {
 				shirt_count[1]++;
-
 			} else if (shirtSize[i].equals("M")) {
 				shirt_count[2]++;
 			} else if (shirtSize[i].equals("L")) {
 				shirt_count[3]++;
 			} else if (shirtSize[i].equals("XL")) {
-				shirt_count[3]++;
+				shirt_count[4]++;
+			}
+		}
+		for(int i=0; i<shirt_count.length; i++) {
+			rank[i]=1;
+			for(int j=0; j<shirt_count.length;j++) {
+				if(shirt_count[i]< shirt_count[j]) {
+					rank[i]=rank[i]+1;
+				}
 			}
 		}
 		return shirt_count;
@@ -32,7 +42,7 @@ public class Shirtsize {
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 		String[] shirtSize = { "XS", "S", "L", "L", "XL", "S" };
-		int[] ret = sol.Solution(shirtSize);
+		int[] ret = sol.solution(shirtSize);
 		System.out.println("Solution : return value of method is" + Arrays.toString(ret));
 
 	}
