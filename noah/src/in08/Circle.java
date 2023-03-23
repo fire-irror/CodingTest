480e3fe7f9dd3bba64b246f926f98f0b613df8a4
parkhwakyoung <firenoah.m@gmail.com>
2023-03-23 09:07:50 +0900

    오버라이딩과 super 키워드

---
+++ noah/src/in08/Circle.java
@@ -0,0 +1,17 @@
+package in08;
+
+public class Circle extends Shape {
+	protected String name;
+	@Override
+	public void draw() {
+		name = "Circle";
+		super.name="Shape";
+		super.draw();
+		System.out.println(name);
+	}
+	public static void main(String[] args) {
+		Shape b = new Circle();
+		b.paint();
+	}
+
+}
