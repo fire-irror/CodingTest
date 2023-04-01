d722b53690cc7f77d8d6a9c670dc54d8e53799a0
parkhwakyoung <firenoah.m@gmail.com>
2023-04-01 23:40:04 +0900

    자바 package 예제

---
+++ Pro01/src/app/GoodCalc.java
@@ -0,0 +1,31 @@
+package app;
+
+import lib.Calculator;
+
+public class GoodCalc{
+
+public int add(int a,int b) {
+	return a+b;
+}
+
+	public int subtract(int a, int b) {
+		// TODO Auto-generated method stub
+		return a-b;
+	}
+	public double  average(int[] a) {  //int a[]=new int[] {2,3,4}와 같은 표현
+		// TODO Auto-generated method stub
+		double sum=0;
+		for(int i=0; i<a.length; i++) 
+			sum+=a[i];
+			return sum/a.length;
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GoodCalc c=new GoodCalc();
+		System.out.println(c.add(2, 3));
+		System.out.println(c.subtract(2, 3));
+		System.out.println(c.average(new int[] {2,3,4}));
+	}
+}
