18c26f1e27ffcbcbe03300c1e5aef043a056eb25
parkhwakyoung <firenoah.m@gmail.com>
2023-04-02 23:02:27 +0900

    cpu 1학년 실습

---
+++ noah/src/noah/Age.java
@@ -0,0 +1,15 @@
+package noah;
+import java.util.Scanner;
+
+public class Age {
+
+	public static void main(String[] args) {
+		
+		int age;
+		Scanner scan= new Scanner(System.in);
+		age=scan.nextInt();
+		System.out.println("내 나이는 "+age+"입니다");
+
+	}
+
+}
