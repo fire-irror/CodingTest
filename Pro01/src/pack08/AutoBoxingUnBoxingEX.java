fe43810aeb0a70add9382fe5a530e6d1d609112c
parkhwakyoung <firenoah.m@gmail.com>
2023-04-06 08:42:10 +0900

    박싱 언박싱 예제

---
+++ Pro01/src/pack08/AutoBoxingUnBoxingEX.java
@@ -0,0 +1,12 @@
+package pack08;
+
+public class AutoBoxingUnBoxingEX {
+	public static void main(String agrs[]) {
+		int n = 10;		//기본 정수형 데이터
+		Integer intObject = n;	//자동 박싱(auto boxing) -- 기본데이터를 객체타입으로 변환해서 저장
+		System.out.println("intObject ="+intObject);
+		
+		int m = intObject +10;	//객체타입 데이터 + 기본데이터==>기본데이터로 저장 
+		System.out.println("m = "+m);
+	}
+}
