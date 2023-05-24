63f40dfbe95215bc3cef6e0490b2dca44f76fb7c
parkhwakyoung <firenoah.m@gmail.com>
2023-05-24 11:18:47 +0900

    blockBinaryCopy예제 풀기

--- Pro04FileInputOutput/src/file04/BlockBinaryCopyEx.java
+++ Pro04FileInputOutput/src/file04/BlockBinaryCopyEx.java
@@ -19,7 +19,7 @@ public class BlockBinaryCopyEx {
 				int n = fi.read(buf);	//fi가 가리키는 파일에서 데이터를 읽어서 
 //				10kb가 buf에 저장하고, 그 길이 값을 n에 저장한다.
 				fo.write(buf,0,n);	//buf임시기억공간에 저장되어 있는 데이터에서
-				//0번째 부터 시작하여 n갯수 만큼을 fo가 가리키는 파일("blockcopyimg.png)에 출력한다	
+				//0번째 부터 시작하여 n갯수 만큼을 fo가 가리키는 파일("blockcopyimg.png)에 출력한다
 				System.out.println("n="+n+",buf.length"+buf.length+"\n");
 				if(n<buf.length)
 					break;
