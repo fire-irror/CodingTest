2282e3df30fb165134ba972f06e48cdaaa9f4861
fire-irror <firenoah.m@gmail.com>
2023-06-15 09:36:06 +0900

    Thread예제

--- Pro05Thread/src/th03/ThreadMainEx.java
+++ Pro05Thread/src/th03/ThreadMainEx.java
@@ -6,7 +6,7 @@ public class ThreadMainEx {
 	public static void main(String args[]) {
 		long id = Thread.currentThread().getId();			//현재 실행되고 있는 스레드(main)의 id
 		String name = Thread.currentThread().getName();		//현재 실행되고 있는 스레드(main)의 이름
-		int Prority = Thread.currentThread().getPriority();	//우선순위의 기본값은 5로 설정이 된다.
+		int Prority = Thread.currentThread().getPriority();	//우선순위의 기본값은 5로 설정이 된다
 		Thread.State s = Thread.currentThread().getState();
 		System.out.println("현재 스레드 이름 : " + name);
 		System.out.println("현재 스레드 id : " + id);
